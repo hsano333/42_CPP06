b1bea6d0fd6e3b5f2b97cf8696ae037efed03ef7
Hiroaki Sano <hsano@student.42tokyo.jp>
2023-02-10 13:16:00 +0900

    completed ex01

---
+++ ex01/include/Data.hpp
@@ -0,0 +1,12 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <cstdint>
+
+struct Data
+{
+    int dataA;
+    int dataB;
+};
+
+#endif
---
+++ ex01/srcs/main.cpp
@@ -0,0 +1,45 @@
+#include <cstdint>
+#include <Data.hpp>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+uintptr_t serialize(Data* ptr)
+{
+    return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+    return reinterpret_cast<Data *>(raw);
+}
+
+
+int main(void)
+{
+    Data testA = {1, 2};
+    Data testB = {-1, -1};
+    Data *ptestA;
+    Data *ptestB;
+
+    uintptr_t resultA;
+    uintptr_t resultB;
+
+    resultA = serialize(&testA);
+    resultB = serialize(&testB);
+
+    ptestA = deserialize(resultA);
+    ptestB = deserialize(resultB);
+    if (ptestA == &testA)
+        cout << "Success testA" << endl;
+    else
+        cout << "Failure testA" << endl;
+    if (ptestB == &testB)
+        cout << "Success testB" << endl;
+    else
+        cout << "Failure testB" << endl;
+
+
+    return (0);
+}
